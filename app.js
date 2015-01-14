e43183970dbc21f7617e46f13c1a71181db446fd
James Oquendo <james.oquendo@gmail.com>
2015-01-14 10:20:51 -0800

    Fix io call and set socket.io client path to original

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 var http = require('http').Server(app);
-var io = require('socket.io')(server);
+var io = require('socket.io')(http);
 
 io.on('connection', function(socket) {
 	console.log('a user connected');
